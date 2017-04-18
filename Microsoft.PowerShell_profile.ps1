685a73c8190a89096420a3f15f30e839407bf764
Matt Ankerson <matt.ankerson@intergen.co.nz>
2017-04-19 11:49:28 +1200

    ps profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,6 +5,8 @@ set-alias ssh "C:/Program Files/OpenSSH-Win64./ssh"
 set-alias touch New-Item
 set-alias msbuild "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
 set-alias chrome "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+Function startBashFunc {bash -cur_console:p1}
+set-alias start-bash startBashFunc
 
 # To edit the Powershell Profile
 # (Not that I'll remember this)
